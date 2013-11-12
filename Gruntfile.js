8db57216fa6c5be147b2e0eead3368408833c8be
arcreative <adam@arcreative.net>
2013-11-12 10:48:54 -0800

    Committed files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        billy_builder: {
+            title: 'ember-request'
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
